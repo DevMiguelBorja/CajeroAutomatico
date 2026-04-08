ab4bd83e55a6d5771b6e0fae40b55e0256a9495f
DevMiguelBorja <miguel.borja876@pascualbravo.edu.co>
2026-04-08 20:25:01 +0000

    ListaDeUsuarios

--- CajeroAutomatico/Program.cs
+++ CajeroAutomatico/Program.cs
@@ -1,96 +1,150 @@
 ﻿using System; 
+using System.Collections.Generic; 
+
+class Cuenta
+{
+    public int Numero { get; set; }
+    public int Pin { get; set; }
+    public double Saldo { get; set; }
+    public List<double> Retiros { get; set; }
+
+    public Cuenta(int numero, int pin, double saldo)
+    {
+        Numero = numero; 
+        Pin = pin; 
+        Saldo = saldo; 
+        Retiros = new List<double>(); 
+    }
+}
 class Program
 {
+    static List<Cuenta> cuentas = new List<Cuenta>(); 
     static void Main()
     {
-        int cuenta; 
-        int pin; 
 
-        int cuentaExistente = 123456; 
-        int pinExistente = 1111; 
-
-        bool cuentaValida = false; 
+        cuentas.Add(new Cuenta(123456, 1111, 500000)); 
+        cuentas.Add(new Cuenta(654321, 2222, 300000)); 
 
         Console.WriteLine("Bienvenido a DEVBANK!!");
 
-        while (!cuentaValida)
+        Cuenta cuentaActual = Login();  
+
+        if (cuentaActual != null)
         {
-            Console.WriteLine("Ingresa tú número de cuenta: "); 
-            cuenta = Convert.ToInt32(Console.ReadLine());             
-
-            if (cuenta == cuentaExistente)
-            {
-                int intentos = 0; 
-                bool pinCorrecto = false; 
-                while (intentos < 3 && !pinCorrecto)
-                {
-                    Console.WriteLine("Ingresa tú PIN: "); 
-                    pin = Convert.ToInt32(Console.ReadLine());  
-                    if (pin == pinExistente)
-                    {
-                        Console.WriteLine("Has iniciado sesión correctamente."); 
-                        pinCorrecto = true; 
-                        cuentaValida = true; 
-
-                        double saldo = 500000; 
-                        int opcion = 0; 
-
-                        while(opcion != 3)
-                        {
-                            Console.WriteLine("-----DEVBANK------"); 
-                            Console.WriteLine("1.- Consultar saldo ---"); 
-                            Console.WriteLine("2.- Retirar dinero ---"); 
-                            Console.WriteLine("3.- Salir ---");
-                            Console.WriteLine("-----SELECCIONA UNA OPCION------");
-                            opcion = Convert.ToInt32(Console.ReadLine()); 
-
-                            switch (opcion)
-                            {
-                                case 1: 
-                                    Console.WriteLine("Su saldo actual es: $" + saldo); 
-                                    break; 
-                                case 2: 
-                                    Console.WriteLine("Ingresa el monto a retirar: ");
-                                    double retiro = Convert.ToDouble(Console.ReadLine()); 
-
-                                    if (retiro <= saldo )
-                                    {
-                                        saldo -= retiro; 
-                                        Console.WriteLine("Retiro exitoso!"); 
-                                        Console.WriteLine("Saldo disponible: $" + saldo); 
-                                    } else
-                                    {
-                                        Console.WriteLine("Saldo insuficiente"); 
-                                    }
-                                    break;     
-                                case 3: 
-                                    Console.WriteLine("Gracias por usar nuestro cajero DevBank"); 
-                                    break;
-                                default: 
-                                    Console.WriteLine("Opción Inválida"); 
-                                    break;     
-                            }       
-
-                        }
-                    } else
-                    {
-                        intentos++; 
-                        Console.WriteLine("PIN Incorrecto");
-                    }
-                }
-                if (!pinCorrecto)
-                {
-                    Console.WriteLine("Cuenta Bloqueada por intentos fallidos!"); 
-                    break; 
-                }
-                
-            } else
-            {
-                Console.WriteLine("La cuenta no existe"); 
-            }
-            
+            Menu(cuentaActual); 
+            GenerarReporte(cuentaActual); 
         }
 
+        static  Cuenta Login(){
+            while (true){
+                Console.WriteLine("Ingresa tu número de cuenta: "); 
+                int numero = Convert.ToInt32(Console.ReadLine()); 
+
+                Cuenta cuenta = cuentas.Find(c => c.Numero == numero); 
+
+                if (cuenta != null)
+                {
+                    int intentos = 0; 
+
+                    while (intentos < 3)
+                    {
+                        Console.Write("Ingresa tu PIN: "); 
+                        int pin = Convert.ToInt32(Console.ReadLine()); 
+
+                        if (pin == cuenta.Pin)
+                        {
+                            Console.WriteLine("Login exitoso!"); 
+                            return cuenta; 
+                        }
+                        else
+                        {
+                            intentos ++; 
+                            Console.WriteLine("PIN incorrecto!"); 
+                        }
+                    }
+
+                    Console.WriteLine("Cuenta bloqueada!"); 
+                    return null; 
+                }
+                else
+                {
+                    Console.WriteLine("cuenta no existe"); 
+                }
+
+            }
+        }
+
+        static void Menu (Cuenta cuenta)
+        {
+            int opcion = 0; 
+
+            while (opcion != 3)
+            {
+                Console.WriteLine("----- DEVBANK -----");
+                Console.WriteLine("1. Consultar saldo");
+                Console.WriteLine("2. Retirar dinero");
+                Console.WriteLine("3. Salir");
+                Console.Write("Seleccione una opción: ");
+
+                opcion = Convert.ToInt32(Console.ReadLine()); 
+
+                switch (opcion)
+                {
+                    case 1: 
+                        Console.WriteLine($"Saldo actual: ${cuenta.Saldo}"); 
+                        break; 
+                    case 2: 
+                        Console.WriteLine("Monto a retirar: "); 
+                        double retiro = Convert.ToDouble(Console.ReadLine()); 
+
+                        if(retiro <= cuenta.Saldo)
+                        {
+                         cuenta.Saldo -= retiro; 
+                            cuenta.Retiros.Add(retiro); 
+                         Console.WriteLine("Rtiro exitoso!");     
+                        }
+                        else
+                        {
+                            Console.WriteLine("Saldo insuficiente");         
+                        }
+                    break; 
+                    case 3: 
+                        Console.WriteLine("Gracias por usar DevBANK"); 
+                        break; 
+                    default: 
+                        Console.WriteLine("Opcion inválida"); 
+                        break;        
+                }
+            }
+        }
+
+        static void GenerarReporte(Cuenta cuenta)
+        {
+            Console.WriteLine("----REPORTE DE TRANSACCIONES -----"); 
+
+            double totalRetirado = 0; 
+            double mayorRetiro = 0; 
+
+            for (int i = 0; i < cuenta.Retiros.Count; i++)
+            {
+                totalRetirado += cuenta.Retiros[i]; 
+
+                if(cuenta.Retiros[i] > mayorRetiro)
+                {
+                    mayorRetiro = cuenta.Retiros[i]; 
+                }
+            }
+
+            int cantidadRetiros = cuenta.Retiros.Count; 
+            double promedio = cantidadRetiros > 0 ? totalRetirado / cantidadRetiros: 0; 
+
+            Console.WriteLine($"Cantidad de retiros: {cantidadRetiros}"); 
+            Console.WriteLine($"Total retirado: {totalRetirado}");
+            Console.WriteLine($"Promedio de retiros: {cantidadRetiros}"); 
+            Console.WriteLine($"Retiro mayor: {mayorRetiro}");  
+            Console.WriteLine($"Saldo Final: ${cuenta.Saldo}");
+        }
+                    
     }
     
 }
