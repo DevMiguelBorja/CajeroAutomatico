aa592729e56275e1ee3c141e658e70d8229fb74b
DevMiguelBorja <miguel.borja876@pascualbravo.edu.co>
2026-05-23 22:36:15 +0000

    Funcion CDT

--- CajeroAutomatico/obj/Debug/net10.0/CajeroAutomatico.AssemblyInfo.cs
+++ CajeroAutomatico/obj/Debug/net10.0/CajeroAutomatico.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CajeroAutomatico")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf7aa88f4ced4b0210d2ab52ea2144da144d03ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd05dc85a9126c9de92eb0290e4a2f0e45c06a2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CajeroAutomatico")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CajeroAutomatico")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
